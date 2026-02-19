b63d01b3f49c458f1a5356242042c39ca53769b5
Schmackos <xanderwitteveen@gmail.com>
2026-02-19 23:49:53 +0100

    chore: bump version to 1.8.8

--- src/config.h
+++ src/config.h
@@ -14,7 +14,7 @@
 // deviceSerialNumber in app_state.h)
 
 // ===== Firmware Version =====
-#define FIRMWARE_VERSION "1.8.7"
+#define FIRMWARE_VERSION "1.8.8"
 
 // ===== GitHub Repository Configuration =====
 #define GITHUB_REPO_OWNER "Schmackos"
